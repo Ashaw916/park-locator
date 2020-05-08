0e80f7eda7de6fba6cfcc8415783078710ae0f3e
ashaw916 <a.s.young530@gmail.com>
2020-05-07 22:23:38 -0700

    added some styling, cleaned up code, added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 
 
-$(document).ready(function () {
 
+
+$(document).ready(function () {
+    //background image
     $("body").css("background-image", "url('assets/image/park.png')");
 
+    //on click for search button
     $("#search-button").on("click", function (event) {
         var locationName = $("#search-input").val();
+        //console logging
         console.log(locationName)
 
 
