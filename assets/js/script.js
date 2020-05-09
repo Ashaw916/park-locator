0606819f0dd58254fc9fcdc85e4f97f00fbca9a0
ashaw916 <a.s.young530@gmail.com>
2020-05-09 10:37:22 -0700

    added map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
 
-
-
-
 $(document).ready(function () {
+    mapboxgl.accessToken = 'pk.eyJ1IjoicHJhYmhkZWVwIiwiYSI6ImNrOXp3aG9mYzBmMnMzamx0eDU5ZzRxd2IifQ.wkiG09WU_O8N0tVlzlb2tA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
+
     //background image
     $("body").css("background-image", "url('assets/image/park.png')");
 
@@ -12,6 +15,7 @@ $(document).ready(function () {
         //console logging
         console.log(locationName)
 
+       
 
     });
 });
