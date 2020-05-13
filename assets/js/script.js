d37f2a484491ee198fc394a1a46e364fdae29336
ashaw916 <a.s.young530@gmail.com>
2020-05-12 22:29:46 -0700

    got the trails api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,105 @@
-    //background image
-    //$("body").css("background-image", "url('assets/image/park.png')");
-
 $(document).ready(function () {
+
+function getSearchLoc(country,state,city){
+
+    console.log(country,state,city)
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://trailapi-trailapi.p.rapidapi.com/?q[city_cont]=" + city + "&q[state_cont]=" + state + "&radius=25&q[country_cont]=" + country + "&limit=25",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
+            "x-rapidapi-key": "e7756eb0e8msh0ee80a6074e6840p18c867jsna31949dd8a7d"
+            }
+        }
+
+    $.ajax(settings)
+    .then(function(response) {
+        console.log(response)
+        response=response;
+
+        var array = response.places;
+        geoJSON(array)
+        
+    })
+
+};
+   //responce.places[0].lat
+
+    function geoJSON(response) {
+        // console.log(lat,lon,name,city,country,state);
+        for (var i = 0; i < response.length; i++) {
+           var parks={ "type": "FeatureCollection",
+                        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates":  [response[i].lat,response[i].lon]
+                },
+                "properties": {
+                    "name": response[i].name,
+                    "city": response[i].city,
+                    "country": response[i].country,
+                    "state": response[i].state
+                }
+           
+            }
+            ]}
+
+            map.addLayer({
+                "id": i,
+                "type": "symbol",
+                // Add a GeoJSON source containing place coordinates and information. 
+                "source": {
+                    "type": "geojson",
+                    "data": parks
+                    },
+                "layout": {
+                "icon-image": "park-15",
+                "icon-allow-overlap": true,
+                }
+            });
+        }
+        console.log(parks)
+    };
+
+
+
+
     //mapbox token
     mapboxgl.accessToken = 'pk.eyJ1IjoicHJhYmhkZWVwIiwiYSI6ImNrOXp3aG9mYzBmMnMzamx0eDU5ZzRxd2IifQ.wkiG09WU_O8N0tVlzlb2tA';
     //display map and style
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v10',
-        center: [125.6, 10.1],
+        center: [38.5816, 10.1],
         zoom: 14
       });
 
-    //array of park object
-    var parks = {
-        "type": "FeatureCollection",
-        "features": [
-            { 
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [-77.034084142948,38.909671288923]
-            },
-            "properties": {
-                "name": "gilman",
-                "address": "924 gilman st",
-                "city": "berkeley DC",
-                "country": "United States",
-                "crossStreet": "at 15th St NW",
-                "postalCode": "20005",
-                "state": "ca"
-            }
-             },
-            {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [125.6, 10.1]
-            },
-            "properties": {
-                "name": "Dinagat Islands"
-            }
-          }
-        ]};
+    //on load fuction to populate map
+    map.on('load', function (e) {
 
-        //on load fuction to populate map
-        map.on('load', function (e) {
-            // Add the data to your map as a layer 
-            map.addLayer({
-              "id": "locations",
-              "type": "symbol",
-              // Add a GeoJSON source containing place coordinates and information. 
-              "source": {
-                "type": "geojson",
-                "data": parks
-              },
-              "layout": {
-                "icon-image": "park-15",
-                "icon-allow-overlap": true,
-              }
-            });
-          });
+        
+        // // Add the data to your map as a layer 
+        // map.addLayer({
+        //     "id": "locations",
+        //     "type": "symbol",
+        //     // Add a GeoJSON source containing place coordinates and information. 
+        //     "source": {
+        //         "type": "geojson",
+        //         "data": parks
+        //         },
+        //     "layout": {
+        //     "icon-image": "park-15",
+        //     "icon-allow-overlap": true,
+        //     }
+        // });
+    });
 
-    //for eah function for obj id
-    // parks.features.forEach(function(park, i){
-    //     park.properties.id = i;
-    //   });
-
-      function buildLocationList(data) {
+    function buildLocationList(data) {
         parks.features.forEach(function(park, i){
           /**
            * Create a shortcut for `store.properties`,
@@ -93,23 +125,43 @@ $(document).ready(function () {
           link.className = 'title';
         //   link.id = "link-" + prop.id;
           link.innerHTML = prop.address;
-      
-
-          
-          
         });
-      }
+    };
+
+    // function flyToPark(currentFeature) {
+    //     map.flyTo({
+    //         center: currentFeature.geometry.coordinates,
+    //         zoom: 15
+    //     });
+    // };
+
+    // function changeLocation() {
+    //        //mapbox token
+    //     mapboxgl.accessToken = 'pk.eyJ1IjoicHJhYmhkZWVwIiwiYSI6ImNrOXp3aG9mYzBmMnMzamx0eDU5ZzRxd2IifQ.wkiG09WU_O8N0tVlzlb2tA';
+    //      //display map and style
+    //     var map = new mapboxgl.Map({
+    //     container: 'map',
+    //     style: 'mapbox://styles/mapbox/light-v10',
+    //     center: [125.6, 10.1],
+    //     zoom: 14
+    //     });
+    // }
 
     //on click for search button
     $("#search-button").on("click", function (event) {
         var locationName = $("#search-input").val();
         $("#search-input").val("");
         //console logging
-        console.log(locationName)
-        // parks.push(locationName);
+        console.log(locationName.split(","))
+        
         $("#listings").empty();
-        buildLocationList();
+        // buildLocationList();
+        
+        var locationParts = locationName.split(",");
+        getSearchLoc(locationParts[2].trim(),locationParts[1].trim(),locationParts[0].trim());
 
+        // geoJSON(response);
+        // flyToPark();
     });
 
 
