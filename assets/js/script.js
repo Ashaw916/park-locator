74a44b9d679d277f43420c8ff5810676b3fdfdd2
ashaw916 <a.s.young530@gmail.com>
2020-05-14 15:18:52 -0700

    added list to sidebar from search results

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,13 +66,41 @@ $(document).ready(function () {
                     "data": parks,
                     },
                 "layout": {
-                    "icon-image": "marker-15",
+                    "icon-image": "park-15",
                     "icon-allow-overlap": true,
+                    "icon-size": 2,
                     }
             });
             console.log(parks)
+
+            parks.features.forEach(function(parks, i){
+                /**
+                 * Create a shortcut for `park.properties`,
+                 * which will be used several times below.
+                **/
+                var prop = parks.properties;
+            
+                /* Add a new listing section to the sidebar. */
+                var listings = document.getElementById('listings');
+                var listing = listings.appendChild(document.createElement('div'));
+                /* Assign a unique `id` to the listing. */
+                listing.id = "listing-" + prop.name;
+                /* Assign the `item` class to each listing for styling. */
+                listing.className = 'item';
+            
+                /* Add details to the individual listing. */
+                var details = listing.appendChild(document.createElement('div'));
+                details.innerHTML = prop.name;
+      
+                /* Add the link to the individual listing created above. */
+                var link = listing.appendChild(document.createElement('a'));
+              //   link.href = '#';
+                link.className = 'title';
+              //   link.id = "link-" + prop.id;
+                link.innerHTML = prop.address;
+              });
         }
-        buildLocationList(response);
+        // buildLocationList(response);
         
     };
 
@@ -112,36 +140,36 @@ $(document).ready(function () {
     //     }
     //  });
 
-    function buildLocationList(parks) {
-        parks.features.forEach(function(parks, i){
-          /**
-           * Create a shortcut for `park.properties`,
-           * which will be used several times below.
-          **/
-          var prop = parks.properties;
+    // function buildLocationList(parks) {
+    //     parks.features.forEach(function(parks, i){
+    //       /**
+    //        * Create a shortcut for `park.properties`,
+    //        * which will be used several times below.
+    //       **/
+    //       var prop = parks.properties;
       
-          /* Add a new listing section to the sidebar. */
-          var listings = document.getElementById('listings');
-          var listing = listings.appendChild(document.createElement('div'));
-          /* Assign a unique `id` to the listing. */
-          listing.id = "listing-" + prop.name;
-          /* Assign the `item` class to each listing for styling. */
-          listing.className = 'item';
+    //       /* Add a new listing section to the sidebar. */
+    //       var listings = document.getElementById('listings');
+    //       var listing = listings.appendChild(document.createElement('div'));
+    //       /* Assign a unique `id` to the listing. */
+    //       listing.id = "listing-" + prop.name;
+    //       /* Assign the `item` class to each listing for styling. */
+    //       listing.className = 'item';
       
-          /* Add details to the individual listing. */
-          var details = listing.appendChild(document.createElement('div'));
-          details.innerHTML = prop.name;
+    //       /* Add details to the individual listing. */
+    //       var details = listing.appendChild(document.createElement('div'));
+    //       details.innerHTML = prop.name;
 
-          /* Add the link to the individual listing created above. */
-          var link = listing.appendChild(document.createElement('a'));
-        //   link.href = '#';
-          link.className = 'title';
-        //   link.id = "link-" + prop.id;
-          link.innerHTML = prop.address;
-        });
-        //logging variables
-        console.log("buildLocationList" + prop,listings,listing,details);
-    };
+    //       /* Add the link to the individual listing created above. */
+    //       var link = listing.appendChild(document.createElement('a'));
+    //     //   link.href = '#';
+    //       link.className = 'title';
+    //     //   link.id = "link-" + prop.id;
+    //       link.innerHTML = prop.address;
+    //     });
+    //     //logging variables
+    //     console.log("buildLocationList" + prop,listings,listing,details);
+    // };
 
     // function flyToPark(currentFeature) {
     //     map.flyTo({
